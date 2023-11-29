5445dac693c47cbdb8f1e693988d8a244da78411
fuatX <fuatyagizaydin@hotmail.com>
2023-11-29 21:54:22 +0300

    added pico css

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "@picocss/pico";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+/*
+- YAPILACAKLAR
+- [x] Pico Css kullanılacak.
+- [ ] Ana Gorsel Olcak
+- [ ] Hemen altta 5 adet küçük görsel olcak
+- [ ] Kucuk Görsellerden bİrine tıklayınca ana görsel değişecek
+- [ ] Görseller Lorem Picsum'dan gelecek
+- [x] Default olarak 100 Nolu ID secilsin
+- [ ] Gorseller ARRAY olarak [200,201,202,203,204] olacak
+*/
